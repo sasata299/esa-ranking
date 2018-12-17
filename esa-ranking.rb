c4e6b67c4d82ee8ec7edd85fa99417192c000c1b
sasata299 <karaage299@gmail.com>
2018-12-17 09:43:09 +0900

    先週1週間の投稿を取ってきて、主な指標を出力する

--- esa-ranking.rb
+++ esa-ranking.rb
@@ -1,4 +1,12 @@
 require 'esa'
 
 client = Esa::Client.new(access_token: ENV['ESA_ACCESS_TOKEN'], current_team: ENV['ESA_TEAM_NAME'])
-p client
+
+last_week_posts = client.posts(created: '>2018-12-10').body['posts']
+last_week_posts.each do |post|
+  post_detail = client.post(post['number'])
+  p post_detail.body['full_name']
+  p post_detail.body['comments_count']
+  p post_detail.body['stargazers_count']
+  p post_detail.body['watchers_count']
+end
